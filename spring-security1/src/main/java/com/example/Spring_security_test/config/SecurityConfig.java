998969a97025128310aebcc3330b3ba7f9572abe
ranjithv88 <ranjithkumar22445588@gmail.com>
2024-08-08 13:28:13 +0530

    date 08-08-2024

--- spring-security1/src/main/java/com/example/Spring_security_test/config/SecurityConfig.java
+++ spring-security1/src/main/java/com/example/Spring_security_test/config/SecurityConfig.java
@@ -27,7 +27,7 @@ public class SecurityConfig {
                 .exceptionHandling(exception -> exception.authenticationEntryPoint(unauthorizedHandler))
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/api/auth/**").permitAll()
+                        auth.requestMatchers("/api/auth/").permitAll()
                                 .requestMatchers("/user").hasAuthority("USER")
                                 .requestMatchers("/dev").hasAuthority("DEVLOPER")
                                 .requestMatchers("/admin").hasAuthority("ADMIN")
